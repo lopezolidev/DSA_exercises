2a4cc12364210ce4f5bbc98ac36364c9b24cdf20
SJLO <olivera9806@gmail.com>
2024-10-21 01:01:45 -0400

    completing ransomNote exercise, completed leetcode beginner path

--- leetcode/beginner/383.RansomNote.cpp
+++ leetcode/beginner/383.RansomNote.cpp
@@ -30,10 +30,52 @@ ransomNote and magazine consist of lowercase English letters.
 */
 
 bool canConstruct(string ransomNote, string magazine) {
-    
+    bool answer = true ;
+
+    int alphabet[26] = {0} ;
+
+    int i = 0 ;
+    while ( i < magazine.size()){
+
+        int value = int( int(magazine[i]) - int('a') ) ;
+
+        alphabet[value]++ ;
+
+        i++ ;
+    } // loop to initialize the hash table with the number of letters that ' magazine ' contains
+
+    i = 0 ;
+
+    while ( i < ransomNote.size()) {
+
+        int valueToSubstract = int( int(ransomNote[i]) - int('a')) ;
+
+        alphabet[valueToSubstract]-- ;
+
+        i++ ;
+    } // loop to substract letters of ' ransomNote ' from the alphabet array made up from ' magazine '
+
+    i = 0 ;
+    while(i < 26) {
+        if( alphabet[i] < 0 ) {
+            answer = false ;
+        }
+
+        i++ ;
+    } // loop to determine when ' magazine ' cannot reconstruc ' ransomNote ' due to a letter (or more) being present in ' ransomNote ' and not in ' magazine '
+
+    return answer ;
 }
 
 int main () {
+
+    bool answer = canConstruct("camelot", "tocomelcat") ;
+
+    if(answer){
+        cout << "TRUE" ;
+    } else {
+        cout << "FALSE" ;
+    }
 
     return 0 ;
 }
