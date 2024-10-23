8739f908addec17a5027a748d10b65c826c328e6
SJLO <olivera9806@gmail.com>
2024-10-22 20:34:57 -0400

    finishing encodedDecoded with double 'pointer'

--- neetcode/arrays_hashing/encodeDecode.cpp
+++ neetcode/arrays_hashing/encodeDecode.cpp
@@ -29,13 +29,67 @@ strs[i] contains only UTF-8 characters.
 
 string encode(vector<string>& strs) {
 
+    string response = "";
+
+    int i = 0 ;
+
+    while( i < strs.size()) {
+        string word = strs[i] ;
+        
+        string wordSize = to_string(word.size()) ;
+
+        response += wordSize + "#" + word ;
+
+        i++ ;
+    }
+
+    cout << "encoded: " << response << endl ;
+
+    return response ;
 }
 
 vector<string> decode(string s) {
 
+    vector<string> result ;
+
+    int i = 0 ; 
+    
+    while( i < s.size() ){
+        
+        int j = i ;
+        // ' j ' will always start at the position of ' i ', that being the position of the start of the number
+
+        while( s[j] != '#'){
+            j++ ;
+        } 
+        // updating the ' j ' pointer until it reaches the ' # ' symbol, that way we know how many characters we need to extract to get the length of the string
+
+        int length = stoi(s.substr(i, j - i)) ; 
+        // size of encoded word 
+
+        string word = s.substr(j + 1, length) ; 
+        // extracting the substring fom word using  substr function, which will extract a string starting from first argument, to ' length ' characters from the string
+
+        result.push_back(word) ;
+        // appending the word to the vector of strings ' result '
+
+        i = j + 1 + length ; //moving to the next number 
+    }
+
+    return result ;
 }
 
-int main() {
+int main() {    
 
-    retunr 0 ;
+    vector<string> strs = {"we","say",":","yes", "!@#$%^&*()" } ;
+
+    string response = encode(strs) ;
+
+    vector<string> answer = decode(response) ;
+
+    for(int i = 0 ; i < answer.size() ; i++ ){
+        cout << answer[i] << " " ;
+    }
+
+    return 0 ;
 }
